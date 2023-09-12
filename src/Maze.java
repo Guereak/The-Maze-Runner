0f4208d59315222024d110dfc50ef0231a2c1fe2
Maxime L <66002207+Guereak@users.noreply.github.com>
2023-09-12 12:33:12 +0200

    Updated code

---
+++ src/Maze.java
@@ -0,0 +1,11 @@
+public class Maze {
+    public Maze(int cols, int rows) {
+        
+        //We generate the nodes
+        for(int i = 0; i < cols; i++) {
+            for(int j = 0; j < rows; j++) {
+                Node n = new Node(); 
+            }
+        }
+    }
+}
