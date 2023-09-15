69b3f888d7c900716e917eb207249c15e20a1a59
Maxime L <66002207+Guereak@users.noreply.github.com>
2023-09-15 13:07:02 +0200

    Added Maze Verifier

--- src/MazeVerifier.java
+++ src/MazeVerifier.java
@@ -12,5 +12,40 @@ public class MazeVerifier {
         return true;
     }
 
+    public static int cellsFourWalls(Maze maze){
+        int count = 0;
+
+        for(int col = 0; col < maze.columns; col++){
+            for(int row = 0; row < maze.rows; row++){
+                if(maze.nodes[col][row].nodeBelow != null && maze.nodes[col][row].nodeLeft != null && maze.nodes[col][row].nodeAbove != null && maze.nodes[col][row].nodeRight != null){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int cellsZeroWalls(Maze maze){
+        int count = 0;
+
+        for(int col = 0; col < maze.columns; col++){
+            for(int row = 0; row < maze.rows; row++){
+                if(maze.nodes[col][row].nodeBelow == null && maze.nodes[col][row].nodeLeft == null && maze.nodes[col][row].nodeAbove == null && maze.nodes[col][row].nodeRight == null){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int circularPaths(Maze m){
+        int count = 0;
+
+        
+
+        return count;
+    }
     
 }
